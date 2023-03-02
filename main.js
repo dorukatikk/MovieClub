3930b334dca79475556b9e8fc64c7c91c016af42
Doruk Atik <dorukatik@hotmail.com>
2023-03-02 21:14:34 +0300

    files have been arranged

--- main.js
+++
@@ -1,8 +0,0 @@
-//hamburger butonu hamburger değerinde saklanır
-const hamburger = document.querySelector('.hamburger');
-//hamburger butonuna basıldığında is active olmasını sağladık
-//bu sayede butona basıldığında animasyon devreye girecek
-hamburger.addEventListener('click', function (){
-this.classList.toggle('is-active');
-}
-)
