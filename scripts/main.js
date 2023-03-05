c805de66904c8f841b120c0b1bc70c1ab2ea12ad
Doruk Atik <dorukatik@hotmail.com>
2023-03-05 15:29:07 +0300

    Slide-Out hamburger menü eklendi

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 //hamburger butonu hamburger değerinde saklanır
 const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav')
 //hamburger butonuna basıldığında is active olmasını sağladık
 //bu sayede butona basıldığında animasyon devreye girecek
 hamburger.addEventListener('click', function (){
-this.classList.toggle('is-active');
+    hamburger.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
 }
 )
